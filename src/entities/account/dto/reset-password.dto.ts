32ea34dd1a74ef31237720b14c42c30799708f43
kevin <kalemone@hotmail.com>
2025-11-26 00:21:37 +0000

    adição de dto de reset password

---
+++ src/entities/account/dto/reset-password.dto.ts
@@ -0,0 +1,12 @@
+import { IsJWT, IsNotEmpty, IsStrongPassword } from "class-validator";
+
+export class ResetPasswordDto {
+    
+    @IsStrongPassword({}, { message: 'Password must be strong' })
+    @IsNotEmpty({ message: 'Password is required' })
+    password: string;
+
+    @IsJWT({ message: 'Token must be a valid JWT' })
+    @IsNotEmpty({ message: 'Token is required' })
+    token: string;
+}
