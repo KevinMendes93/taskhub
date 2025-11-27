57faa2365eb2b8fdd9b26e56188b4447ea214364
mendeskom <mendeskom@fab.mil.br>
2025-11-27 17:07:36 +0000

    chore: remove .next folder from git

--- taskhub-frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ViaGknDry5lUF3NstukwKqzwu6GWTrQ3E9A8G8bKjpY=\"\n}"
--- taskhub-frontend/.next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__36b083e1._.css",
-  "static/chunks/node_modules_a04d71c1._.js",
-  "static/chunks/lib_310bb0c8._.js"
-],
-    source: "dynamic"
-});
